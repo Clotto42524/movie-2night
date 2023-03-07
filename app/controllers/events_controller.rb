e3be100ac0f0f104dba395ca57eb4eedc7c82e5d
jack ashton <jackdashton@outlook.com>
2023-03-07 11:22:20 +0100

    minor changes to controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,20 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :address)
+  end
 end
