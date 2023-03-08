53b9e8fa565d4f5679426399cf6f8e73a9006021
jack ashton <jackdashton@outlook.com>
2023-03-08 15:56:19 +0100

    added the controller, js and stimulus for genre page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SetGenreController from "./set_genre_controller"
+application.register("set-genre", SetGenreController)
