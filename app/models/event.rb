ebc040fee50ba6b017a754b7420f665b353a8738
Clyde Choi <clydechoi@gmail.com>
2023-03-12 16:51:46 +0100

    address autocomplete w/ styling

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :selected_movies, dependent: :destroy
   has_many :invitations, dependent: :destroy
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   # validates :name, :address, :date, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_113424) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_12_151037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_113424) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
