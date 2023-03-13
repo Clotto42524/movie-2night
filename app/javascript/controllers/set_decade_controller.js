75139ed00f9e4bfccc38202d7e37c7883dd28025
jack ashton <jackdashton@outlook.com>
2023-03-13 15:16:17 +0100

    changes

--- app/javascript/controllers/set_decade_controller.js
+++ app/javascript/controllers/set_decade_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="set-decade"
 export default class extends Controller {
   connect() {
-    // console.log("connected")
   }
 
   toggleButton(event) {
