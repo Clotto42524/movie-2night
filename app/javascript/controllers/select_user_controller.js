47ed02fd8cc0d5c3569adad8706ab28d3e227fe4
jack ashton <jackdashton@outlook.com>
2023-03-13 18:20:34 +0100

    js issues, users updated

---
+++ app/javascript/controllers/select_user_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="set-genre"
+export default class extends Controller {
+  static targets = ["button"]
+
+  connect() {
+    console.log("connnected")
+  }
+}
