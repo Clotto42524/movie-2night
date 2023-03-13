d211f2458003beab74e852ffa8b203b82417b16a
jack ashton <jackdashton@outlook.com>
2023-03-13 16:53:12 +0100

    finished genre

--- app/javascript/controllers/set_decade_controller.js
+++ app/javascript/controllers/set_decade_controller.js
@@ -2,10 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="set-decade"
 export default class extends Controller {
+
   connect() {
   }
 
   toggleButton(event) {
-    event.currentTarget.classList.toggle("active")
+    this.currentTarget.classList.toggle("active")
   }
 }
