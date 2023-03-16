a7cc6304af2f79435889a1694a8b3bf35775bc6a
Clyde Choi <clydechoi@gmail.com>
2023-03-16 09:40:21 +0100

    change date in events

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_092516) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_082609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string "name"
-    t.date "date"
+    t.datetime "date", precision: nil
     t.string "address"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
